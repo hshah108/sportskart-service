a41da46a151d8a31546be410264bea8197718331
hardik <hardik0109@gmail.com>
2018-10-28 00:01:51 +0530

    resolving CORS issue

--- src/main/java/com/sportskart/sportskartservice/controller/CategoryController.java
+++ src/main/java/com/sportskart/sportskartservice/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.sportskart.sportskartservice.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.sportskart.sportskartservice.model.Category;
 import com.sportskart.sportskartservice.service.CategoryService;
 
 @RestController
+@CrossOrigin(origins="*")
 public class CategoryController {
 
 	@Autowired
