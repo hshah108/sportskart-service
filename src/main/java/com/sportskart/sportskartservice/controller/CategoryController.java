13c9a3af54ba18f235a6d5f40c1361c138f27394
hardik <hardik0109@gmail.com>
2018-11-25 20:21:06 +0530

    adding zipkin and sleuth support

--- src/main/java/com/sportskart/sportskartservice/controller/CategoryController.java
+++ src/main/java/com/sportskart/sportskartservice/controller/CategoryController.java
@@ -2,6 +2,8 @@ package com.sportskart.sportskartservice.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sportskart.sportskartservice.model.Category;
 import com.sportskart.sportskartservice.service.CategoryService;
 
+
 @RestController
 @CrossOrigin(origins="*")
 public class CategoryController {
@@ -17,9 +20,12 @@ public class CategoryController {
 	@Autowired
 	CategoryService categoryService;
 	
+	private Logger logger = LoggerFactory.getLogger(CategoryController.class);
 	@GetMapping("/categories")
 	public List<Category> getAllCategories(){
-		System.out.println("Hello World");
+		logger.debug("calling category service ");
+		List<Category> categories = categoryService.getAllCategories();
+		logger.debug("retrieved categories : " + categories);
 		return categoryService.getAllCategories();
 	}
 	
